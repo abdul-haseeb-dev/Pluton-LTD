7cf7c9bfeba366b72b2c24e48eb5685e24aeb948
Abdul Haseeb Hamid <111380595+abdul-haseeb-dev@users.noreply.github.com>
2023-01-18 09:36:19 +0500

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-pragma solidity ^0.8.0;
-
 contract Collection is ERC721Enumerable, Ownable {
 
     struct TokenInfo {
